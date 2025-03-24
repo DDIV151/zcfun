3eb48ff5ba0cc258018fa9a6161534d2481c3000
DDIV151 <ddiv151@163.com>
2025-03-25 00:05:39 +0800

    Initial commit

---
+++ src/test/java/com/ddiv/zcfun/ZcfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiv.zcfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
